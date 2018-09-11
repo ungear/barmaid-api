d7609db29ade9341bef3a1c7f71a45c68ce5e424
Mikhail Istomin <redeemer@inbox.ru>
2018-09-11 22:27:43 +0300

    save drink thumb image url; prettier

--- db/dbService.js
+++ db/dbService.js
@@ -1,42 +1,57 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const mongoose = require("mongoose");
-const dbUrl = require('./dbConfig.js').DB_URL;
+const dbUrl = require("./dbConfig.js").DB_URL;
 const Schema = mongoose.Schema;
 
 const DrinkScheme = new Schema({
   name: String,
   id: Number,
-})
+  thumbImageUrl: String
+});
 const Drink = mongoose.model("drinks", DrinkScheme);
 
-exports.upsertDrinks = function(drinks){
-  mongoose.connect(dbUrl,{ useNewUrlParser: true });
-  console.log(drinks)
+exports.upsertDrinks = function(drinks) {
+  mongoose.connect(
+    dbUrl,
+    { useNewUrlParser: true }
+  );
+  console.log(drinks);
   let upsertPromises = drinks.map(drinkData => {
     let drink = {
       name: drinkData.strDrink,
       id: drinkData.idDrink,
-    }
-    return Drink.update({id: drink.id}, drink, {upsert: true})
-  })
+      thumbImageUrl: drinkData.strDrinkThumb
+    };
+    return Drink.update({ id: drink.id }, drink, { upsert: true });
+  });
 
   return Promise.all(upsertPromises)
-    .then(_ => {debugger; mongoose.disconnect()})
-    .catch(e => {debugger; mongoose.disconnect()})
-}
+    .then(_ => {
+      debugger;
+      mongoose.disconnect();
+    })
+    .catch(e => {
+      debugger;
+      mongoose.disconnect();
+    });
+};
 
-exports.getDrinkNames = function({like}){
-  mongoose.connect(dbUrl,{ useNewUrlParser: true });
+exports.getDrinkNames = function({ like }) {
+  mongoose.connect(
+    dbUrl,
+    { useNewUrlParser: true }
+  );
   let query = like
-    ? Drink.find({name: new RegExp(like, "i")}, "name")
-    : Drink.find({}, "name")
-    
+    ? Drink.find({ name: new RegExp(like, "i") }, "name")
+    : Drink.find({}, "name");
+
   return query
     .exec()
-    .then(results => { 
-      mongoose.disconnect(); 
-      return results.map(x => x.name);})
-    .catch(e => {
-      mongoose.disconnect()
+    .then(results => {
+      mongoose.disconnect();
+      return results.map(x => x.name);
     })
-}
+    .catch(e => {
+      mongoose.disconnect();
+    });
+};
