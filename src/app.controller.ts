b729f1660db50bcfb56fac64f10918e6067dbb98
Mikhail Istomin <mikhail_istomin@epam.com>
2024-02-03 23:14:14 +0400

    add prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DbService } from './db/db.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    const a = await this.dbService.drink.findFirst();
+    return a; //this.appService.getHello();
   }
 }
