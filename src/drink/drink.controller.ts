48c62dbdda4c718ec1f71b60e99fa258421933c1
Mikhail Istomin <redeemer@inbox.ru>
2024-04-20 20:31:10 +0400

    add Swagger with basic info

--- src/drink/drink.controller.ts
+++ src/drink/drink.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { DrinkService } from './drink.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('drink')
 export class DrinkController {
   constructor(private drinkService: DrinkService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all drinks' })
   async getAllDrinks(): Promise<any> {
     return this.drinkService.getAllDrinks();
   }
 
   @Get('/search')
+  @ApiOperation({ summary: 'Search drinks by name' })
   async searchDrinks(@Query('name') name: string): Promise<any> {
     return this.drinkService.getDrinks(name);
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Get a drink by id' })
   async getDrinkById(@Param('id', ParseIntPipe) id: number): Promise<any> {
     return this.drinkService.getDrinkById(id);
   }
--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { IngredientService } from './ingredient.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('ingredient')
 export class IngredientController {
   constructor(private ingredientService: IngredientService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all ingredients' })
   async getAllIngredients(): Promise<any> {
     return this.ingredientService.getAllIngredients();
   }
