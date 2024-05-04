ef03442edd2157a62421759650883b15ab6d5033
Mikhail Istomin <redeemer@inbox.ru>
2024-05-04 13:21:43 +0400

    remove redundant /summary endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,23 +10,9 @@ export class AppController {
     private dbService: DbService,
   ) {}
 
-  summaryCache = null;
-
   @Get()
   async getHello(): Promise<any> {
     const a = await this.dbService.drink.findFirst();
     return a; //this.appService.getHello();
-  }
-
-  @Get("/summary")
-  @ApiOperation({ summary: 'Number of drinks, ingredients and alc types in DB' })
-  async getSummary(): Promise<any> {
-    if(this.summaryCache) return this.summaryCache;
-
-    const drinksCount = await this.dbService.drink.count();
-    const ingredientsCount = await this.dbService.ingredient.count();
-
-    this.summaryCache = { drinks: drinksCount, ingredients: ingredientsCount};
-    return this.summaryCache;
   }
 }
