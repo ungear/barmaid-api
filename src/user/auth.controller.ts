3e3c2ba1b6184c6b171b181ef51c9296e9c7287c
Mikhail Istomin <redeemer@inbox.ru>
2024-06-02 19:35:28 +0400

    feat(auth): set JWT as cookie

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException, Res } from '@nestjs/common';
+import {Response} from "express";
 import { JwtPayload, LoginDto } from './dto';
 import { ApiBody, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { COOKIE_MAX_AGE_MS, COOKIE_NAME } from './authSettings';
 
 @Controller('auth')
 export class AuthController {
@@ -11,16 +13,17 @@ export class AuthController {
   @Post('login')
   @ApiOperation({ summary: 'Sign in' })
   @ApiBody({ type: LoginDto })
-  async login(@Body() params: LoginDto) {
+  async login(@Body() params: LoginDto, @Res({ passthrough: true }) response: Response) {
     const user = await this.authService.validateUser(params.login, params.password);
     if(!user) throw new UnauthorizedException();
     
 
     const payload: JwtPayload = { userId: user.id };
-    const resp = {
-      access_token: await this.jwtService.signAsync(payload),
-    };
-    return resp;
+    const signedJwt = await this.jwtService.signAsync(payload);
 
+    response.cookie(COOKIE_NAME, signedJwt, {
+      httpOnly: true,
+      maxAge: COOKIE_MAX_AGE_MS, 
+    });
   }
 }
--- src/user/authSettings.ts
+++ src/user/authSettings.ts
@@ -1,3 +1,9 @@
+export const AUTH_SESSION_LIFETIME_S = 60;
+
 export const JWT_SECRET="JWT_SECRET"
 
-export const JWT_EXPIRATION="60s"
+export const JWT_EXPIRATION=`${AUTH_SESSION_LIFETIME_S}s`;
+
+export const COOKIE_NAME="barmaid-auth"
+
+export const COOKIE_MAX_AGE_MS=AUTH_SESSION_LIFETIME_S*1000;
