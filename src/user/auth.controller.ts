3f459fbcd690465222f245d3b8f074fbfa2a35e5
Mikhail Istomin <redeemer@inbox.ru>
2024-06-08 17:13:36 +0400

    feat(auth): update cookie properties one more time

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -25,7 +25,7 @@ export class AuthController {
       httpOnly: true,
       maxAge: COOKIE_MAX_AGE_MS, 
       domain: COOKIE_DOMAIN,
-      sameSite: true,
+      sameSite: false,
     });
   }
 }
